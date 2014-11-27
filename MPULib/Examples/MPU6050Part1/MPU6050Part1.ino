c4259e4520914e5c388dc2eccabe329d71c17b10
Johan Work <johan@gummesson.org>
2014-11-27 14:51:06 +0100

    Added inv_mpu.h/cpp

--- MPULib/Examples/MPU6050Part1/MPU6050Part1.ino
+++ MPULib/Examples/MPU6050Part1/MPU6050Part1.ino
@@ -1,4 +1,6 @@
 /*====================================================================
+The MIT License (MIT)
+
 Copyright (c) 2014 Johan Gummesson
 
 Permission is hereby granted, free of charge, to any person obtaining
